6d0ec86d623881b55407443bb777894bc2e7d8e6
Alan69 <gameg1676@gmail.com>
2024-06-11 00:43:40 +0600

    last orders page added

--- app.py
+++ app.py
@@ -1,15 +1,28 @@
 from flask import Flask, render_template, request, redirect, url_for, flash
 import requests
-from config import Config
 from flask_bootstrap import Bootstrap4
 from alpaca.trading.client import TradingClient
 from alpaca.trading.requests import MarketOrderRequest
 from alpaca.trading.enums import OrderSide, TimeInForce
+from alpaca.trading.requests import GetOrdersRequest
+from alpaca.trading.enums import QueryOrderStatus
+from alpaca.trading.enums import AssetClass
+from dotenv import load_dotenv
+import os
 
-trading_client = TradingClient(Config.ALPACA_API_KEY, Config.ALPACA_SECRET_KEY, paper=True)
+load_dotenv('.env.local')
+
+ALPACA_API_KEY = os.getenv('ALPACA_API_KEY')
+ALPACA_SECRET_KEY = os.getenv('ALPACA_SECRET_KEY')
+ALPACA_BASE_URL = os.getenv('ALPACA_BASE_URL')
+
+trading_client = TradingClient(ALPACA_API_KEY, ALPACA_SECRET_KEY, paper=True)
 
 app = Flask(__name__)
-app.config.from_object(Config)
+app.config['ALPACA_API_KEY'] = ALPACA_API_KEY
+app.config['ALPACA_SECRET_KEY'] = ALPACA_SECRET_KEY
+app.config['ALPACA_BASE_URL'] = ALPACA_BASE_URL
+app.secret_key = 'supersecretkey'  # For flashing messages
 Bootstrap4(app)
 
 @app.route('/')
@@ -23,7 +36,17 @@ def fetch_assets():
             'Apca-Api-Key-Id': app.config['ALPACA_API_KEY'],
             'Apca-Api-Secret-Key': app.config['ALPACA_SECRET_KEY'],
         }
-        response = requests.get(f"{app.config['ALPACA_BASE_URL']}/v2/assets", headers=headers, params={'asset_class': 'crypto'})
+
+        params = {
+            'asset_class': AssetClass.US_EQUITY,  # Adjust if needed
+            'status': 'active',
+            'tradable': True,
+            'page_size': 10,
+            'style': 'american',
+            'limit': 10
+        }
+
+        response = requests.get(f"{app.config['ALPACA_BASE_URL']}/v2/assets", headers=headers, params=params)
         assets = response.json()
         return render_template('assets.html', assets=assets)
     except Exception as e:
@@ -51,6 +74,20 @@ def create_trade():
 
     return redirect(url_for('index'))
 
+@app.route('/orders', methods=['GET'])
+def fetch_orders():
+    try:
+        get_orders_data = GetOrdersRequest(
+        status=QueryOrderStatus.CLOSED,
+        limit=100,
+        nested=True  # show nested multi-leg orders
+    )
+
+        orders=trading_client.get_orders(filter=get_orders_data)
+        return render_template('orders.html', orders=orders)
+    except Exception as e:
+        flash(f"Error fetching orders: {e}", 'danger')
+        return redirect(url_for('index'))
+
 if __name__ == '__main__':
-    app.secret_key = 'supersecretkey'  # For flashing messages
     app.run(debug=True)
